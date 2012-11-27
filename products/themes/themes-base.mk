50257584432a031f05c7cef8409edf3edb8b2aa2
Jesús David <jedga_95@outlook.com>
2012-11-27 00:43:39 -0500

    First commit
    Join everything from tmo on a single repo

---
+++ products/themes/themes-base.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += \
+	ThemeManager \
+	ThemeChooser \
+	com.tmobile.themes \
+	Androidian \
+	Cyanbread
+
+PRODUCT_COPY_FILES := \
+	frameworks/base/data/etc/com.tmobile.software.themes.xml:/system/etc/permissions/com.tmobile.software.themes.xml
